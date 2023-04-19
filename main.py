b481cdb31ba1b5c86a08f8de8faed7cf811d6b7e
aslanova8 <aslanova.polina@inbox.ru>
2023-04-19 15:03:09 +0300

    Multiplayer mode

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 
-import telegram as telegram
 from aiogram import Bot, Dispatcher, executor
 from aiogram.types import Message
 
@@ -10,30 +9,25 @@ BOT_TOKEN: str = '5915056188:AAHIMEHYEG_sxcQyNEfIc5IB_y0of-Zzf3Y'
 bot: Bot = Bot(BOT_TOKEN)
 dp: Dispatcher = Dispatcher(bot)
 
-# Количество попыток, доступных пользователю в игре
-ATTEMPTS: int = 5
-WORD_LEN: int = 5
+#
+file_len_5 = open(f'words_len_5.txt', 'r', encoding='utf-8')
+L_WORDS: list = [line.strip() for line in file_len_5.readlines()]
+file_len_5.close()
 
-file = open(f'words_len_{WORD_LEN}.txt', 'r', encoding='utf-8')
-L_WORDS: list = [line.strip() for line in file.readlines()]
-file.close()
-
-# Словарь, в котором будут храниться данные пользователя
-user: dict = {'in_game': False,
-              'secret_word': None,
-              'attempts': None,
-              'total_games': 0,
-              'wins': 0}
-
-
-# Функция возвращающая случайное целое слово
-def get_random_word() -> str:
-    return random.choice(L_WORDS)
+# Словарь, в котором будут храниться данные пользователей
+users: dict = {}
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: Message):
+    if users.get("{0}".format(message.chat.id), False) is False:
+        users["{0}".format(message.chat.id)] = {'in_game': False,
+                                                'secret_word': None,
+                                                'attempts': 5,
+                                                'word_len': 5,
+                                                'total_games': 0,
+                                                'wins': 0}
     await message.answer('Привет!\nДавай сыграем в игру "Угадай слово"?\n\n'
                          'Чтобы получить правила игры и список доступных '
                          'команд - отправьте команду /help')
@@ -42,8 +36,9 @@ async def process_start_command(message: Message):
 # Этот хэндлер будет срабатывать на команду "/help"
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: Message):
-    await message.answer(f'Правила игры:\n\nЯ загадываю слово из {WORD_LEN} букв, '
-                         f'а вам нужно его угадать\nУ вас есть {ATTEMPTS} '
+    await message.answer(f'Правила игры:\n\nЯ загадываю слово из '
+                         f'{users["{0}".format(message.chat.id)]["word_len"]} букв, '
+                         f'а вам нужно его угадать\nУ вас есть {users["{0}".format(message.chat.id)]["attempts"]} '
                          f'попыток\n\nДоступные команды:\n/help - правила '
                          f'игры и список команд\n/cancel - выйти из игры\n'
                          f'/stat - посмотреть статистику\n\nДавай сыграем?')
@@ -52,85 +47,86 @@ async def process_help_command(message: Message):
 # Этот хэндлер будет срабатывать на команду "/stat"
 @dp.message_handler(commands=['stat'])
 async def process_stat_command(message: Message):
-    await message.answer(f'Всего игр сыграно: {user["total_games"]}\n'
-                         f'Игр выиграно: {user["wins"]}')
+    await message.answer(f'Всего игр сыграно: {users["{0}".format(message.chat.id)]["total_games"]}\n'
+                         f'Игр выиграно: {users["{0}".format(message.chat.id)]["wins"]}')
 
 
 # Этот хэндлер будет срабатывать на команду "/cancel"
 @dp.message_handler(commands=['cancel'])
 async def process_cancel_command(message: Message):
-    if user['in_game']:
+    if users["{0}".format(message.chat.id)]['in_game']:
         await message.answer('Вы вышли из игры. Если захотите сыграть '
                              'снова - напишите об этом')
-        user['in_game'] = False
+        users["{0}".format(message.chat.id)]['in_game'] = False
     else:
         await message.answer('А мы итак с вами не играем. '
                              'Может, сыграем разок?')
 
 
 # Этот хэндлер будет срабатывать на согласие пользователя сыграть в игру
-@dp.message_handler(lambda message: message.text.upper() in ['ДА', 'Давай', 'Сыграем', 'Игра',
-                                                     'Играть', 'Хочу играть'])
+@dp.message_handler(lambda message: message.text.strip().upper() in ['ДА', 'ДАВАЙ', 'СЫГРАЕМ', 'ИГРА',
+                                                                     'ИГРАТЬ', 'ХОЧУ ИГРАТЬ'])
+# TODO: Обрезать знаки препинания
 async def process_positive_answer(message: Message):
-    if not user['in_game']:
+    if not users["{0}".format(message.chat.id)]['in_game']:
         await message.answer('Ура!\n\nЯ загадал слово,'
                              'попробуй угадать!')
-        user['in_game'] = True
-        user['secret_word'] = get_random_word()
-        user['attempts'] = ATTEMPTS
+        users["{0}".format(message.chat.id)]['in_game'] = True
+        users["{0}".format(message.chat.id)]['secret_word'] = random.choice(L_WORDS)
+        users["{0}".format(message.chat.id)]['attempts'] = users["{0}".format(message.chat.id)]["attempts"]
     else:
         await message.answer('Пока мы играем в игру я могу '
                              'реагировать только на слова '
-                             f'из {WORD_LEN} букв '
+                             f'из {users["{0}".format(message.chat.id)]["word_len"]} букв '
                              'и команды /cancel и /stat')
 
 
 # Этот хэндлер будет срабатывать на отказ пользователя сыграть в игру
-@dp.message_handler(lambda message: message.text in ['Нет', 'Не', 'Не хочу', 'Не буду'])
+@dp.message_handler(lambda message: message.text.upper() in ['НЕТ', 'НЕ', 'НЕ ХОЧУ', 'НЕ БУДУ', 'ВЫХОД'])
 async def process_negative_answer(message: Message):
-    if not user['in_game']:
-        await message.answer('Жаль :(\n\nЕсли захотите поиграть - просто '
-                             'напишите об этом')
-    else:
-        await message.answer('Мы же сейчас с вами играем. Присылайте, '
-                             f'пожалуйста, слова из {WORD_LEN} букв')
+    await message.answer('Жаль :(\n\nЕсли захотите поиграть - просто напишите об этом')
 
 
 # Этот хэндлер будет срабатывать на отправку пользователем слов нужной длины
-@dp.message_handler(lambda message: message.text.isalpha() and len(message.text) == WORD_LEN)
-async def process_numbers_answer(message: Message):
-    if user['in_game']:
-        if str(message.text) == user['secret_word']:
+@dp.message_handler(
+    lambda message: message.text.isalpha() and len(message.text) == users["{0}".format(message.chat.id)]["word_len"])
+async def process_word_answer(message: Message):
+    if users["{0}".format(message.chat.id)]['in_game']:
+
+        # Победа
+        if str(message.text).upper() == users["{0}".format(message.chat.id)]['secret_word']:
             await message.answer('Ура!!! Вы угадали слово!\n\n'
                                  'Может, сыграем еще?')
-            user['in_game'] = False
-            user['total_games'] += 1
-            user['wins'] += 1
-        else:
-            w = ''
-            sig = ''
-            for letter1, letter2 in zip(user['secret_word'], str(message.text).upper()):
-                if letter1 == letter2:
-                    w += '<b>' + letter2.upper() + '</b>'
-                    sig += '*'
-                elif letter2 in user['secret_word']:
-                    w += '<u>'+letter2.upper() + '</u>'
-                    sig += '-'
-                else:
-                    w += letter2.upper()
-                    sig += ' '
-            await message.answer(f''
-                                 f' {w}\n'
-                                 f' {sig}', parse_mode="HTML")
-            user['attempts'] -= 1
+            users["{0}".format(message.chat.id)]['in_game'] = False
+            users["{0}".format(message.chat.id)]['total_games'] += 1
+            users["{0}".format(message.chat.id)]['wins'] += 1
 
-        if user['attempts'] == 0:
+        # Попытка
+        else:
+            mistery: str = users["{0}".format(message.chat.id)][
+                'secret_word']  # Шаблон для удаления букв в случае повторяющихся букв в слове-загадке
+            w = ''  # Слово-ответ с подсветкой
+            for letter1, letter2 in zip(users["{0}".format(message.chat.id)]['secret_word'], str(message.text).upper()):
+                if letter1 == letter2:
+                    w += '<u>' + letter2.upper() + '</u>'
+                    mistery = mistery.replace(letter2.upper(), '', 1)
+                elif letter2 in mistery:
+                    w += '<b>' + letter2.upper() + '</b>'
+                    mistery = mistery.replace(letter2.upper(), '', 1)
+                else:
+                    w += '<s>' + letter2.upper() + '</s>'
+
+            await message.answer(f''
+                                 f' {w}', parse_mode="HTML")
+            users["{0}".format(message.chat.id)]['attempts'] -= 1
+
+        if users["{0}".format(message.chat.id)]['attempts'] == 0:
             await message.answer(f'К сожалению, у вас больше не осталось '
                                  f'попыток. Вы проиграли :(\n\nМое слово '
-                                 f'было {user["secret_word"]}\n\nДавайте '
+                                 f'было {users["{0}".format(message.chat.id)]["secret_word"]}\n\nДавайте '
                                  f'сыграем еще?')
-            user['in_game'] = False
-            user['total_games'] += 1
+            users["{0}".format(message.chat.id)]['in_game'] = False
+            users["{0}".format(message.chat.id)]['total_games'] += 1
     else:
         await message.answer('Мы еще не играем. Хотите сыграть?')
 
@@ -138,7 +134,7 @@ async def process_numbers_answer(message: Message):
 # Этот хэндлер будет срабатывать на остальные любые сообщения
 @dp.message_handler()
 async def process_other_text_answers(message: Message):
-    if user['in_game']:
+    if users["{0}".format(message.chat.id)]['in_game']:
         await message.answer('Мы же сейчас с вами играем. '
                              'Присылайте, пожалуйста, слово.')
     else:
