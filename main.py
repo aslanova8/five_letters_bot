8126fc8f5a9f98e9ed34f7030e26a688a0b0fc3e
aslanova8 <aslanova.polina@inbox.ru>
2023-04-30 12:40:16 +0300

    Fix the error of highlighting repeated letters in a word

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ dp: Dispatcher = Dispatcher(bot)
 
 #
 file_len_5 = open(f'words_len_5.txt', 'r', encoding='utf-8')
-L_WORDS: list = [line.strip() for line in file_len_5.readlines()]
+L_WORDS_5: list = [line.strip() for line in file_len_5.readlines()]
 file_len_5.close()
 
 # Словарь, в котором будут храниться данные пользователей
@@ -24,7 +24,8 @@ async def process_start_command(message: Message):
     if users.get("{0}".format(message.chat.id), False) is False:
         users["{0}".format(message.chat.id)] = {'in_game': False,
                                                 'secret_word': None,
-                                                'attempts': 5,
+                                                'total_attempts': 5,
+                                                'temp_attempts': 5,
                                                 'word_len': 5,
                                                 'total_games': 0,
                                                 'wins': 0}
@@ -38,7 +39,7 @@ async def process_start_command(message: Message):
 async def process_help_command(message: Message):
     await message.answer(f'Правила игры:\n\nЯ загадываю слово из '
                          f'{users["{0}".format(message.chat.id)]["word_len"]} букв, '
-                         f'а вам нужно его угадать\nУ вас есть {users["{0}".format(message.chat.id)]["attempts"]} '
+                         f'а вам нужно его угадать\nУ вас есть {users["{0}".format(message.chat.id)]["total_attempts"]}'
                          f'попыток\n\nДоступные команды:\n/help - правила '
                          f'игры и список команд\n/cancel - выйти из игры\n'
                          f'/stat - посмотреть статистику\n\nДавай сыграем?')
@@ -64,27 +65,25 @@ async def process_cancel_command(message: Message):
 
 
 # Этот хэндлер будет срабатывать на согласие пользователя сыграть в игру
-@dp.message_handler(lambda message: message.text.strip().upper() in ['ДА', 'ДАВАЙ', 'СЫГРАЕМ', 'ИГРА',
+@dp.message_handler(lambda message: message.text.strip().upper() in ['ДА', 'ДАВАЙ', 'СЫГРАЕМ', 'ИГРА', 'ДАВАЙ ИГРАТЬ',
                                                                      'ИГРАТЬ', 'ХОЧУ ИГРАТЬ'])
 # TODO: Обрезать знаки препинания
+# TODO Система подсказок
 async def process_positive_answer(message: Message):
-    if not users["{0}".format(message.chat.id)]['in_game']:
-        await message.answer('Ура!\n\nЯ загадал слово,'
-                             'попробуй угадать!')
-        users["{0}".format(message.chat.id)]['in_game'] = True
-        users["{0}".format(message.chat.id)]['secret_word'] = random.choice(L_WORDS)
-        users["{0}".format(message.chat.id)]['attempts'] = users["{0}".format(message.chat.id)]["attempts"]
-    else:
-        await message.answer('Пока мы играем в игру я могу '
-                             'реагировать только на слова '
-                             f'из {users["{0}".format(message.chat.id)]["word_len"]} букв '
-                             'и команды /cancel и /stat')
+    await message.answer('Ура!\n\nЯ загадал слово, попробуй угадать!')
+    users["{0}".format(message.chat.id)]['in_game'] = True
+    users["{0}".format(message.chat.id)]['secret_word'] = random.choice(L_WORDS_5)
+    users["{0}".format(message.chat.id)]['temp_attempts'] = users["{0}".format(message.chat.id)]["total_attempts"]
 
 
 # Этот хэндлер будет срабатывать на отказ пользователя сыграть в игру
 @dp.message_handler(lambda message: message.text.upper() in ['НЕТ', 'НЕ', 'НЕ ХОЧУ', 'НЕ БУДУ', 'ВЫХОД'])
 async def process_negative_answer(message: Message):
-    await message.answer('Жаль :(\n\nЕсли захотите поиграть - просто напишите об этом')
+    if users["{0}".format(message.chat.id)]['in_game']:
+        await message.answer(f'Было загадано слово {users["{0}".format(message.chat.id)]["secret_word"]}.'
+                             f'\n\nЕсли захотите поиграть - просто напишите об этом!')
+    else:
+        await message.answer('Жаль :(\n\nЕсли захотите поиграть - просто напишите об этом')
 
 
 # Этот хэндлер будет срабатывать на отправку пользователем слов нужной длины
@@ -103,24 +102,34 @@ async def process_word_answer(message: Message):
 
         # Попытка
         else:
-            mistery: str = users["{0}".format(message.chat.id)][
-                'secret_word']  # Шаблон для удаления букв в случае повторяющихся букв в слове-загадке
-            w = ''  # Слово-ответ с подсветкой
-            for letter1, letter2 in zip(users["{0}".format(message.chat.id)]['secret_word'], str(message.text).upper()):
-                if letter1 == letter2:
-                    w += '<u>' + letter2.upper() + '</u>'
-                    mistery = mistery.replace(letter2.upper(), '', 1)
-                elif letter2 in mistery:
-                    w += '<b>' + letter2.upper() + '</b>'
-                    mistery = mistery.replace(letter2.upper(), '', 1)
-                else:
-                    w += '<s>' + letter2.upper() + '</s>'
+            # Проверка слова на существование
+            if str(message.text).upper() in L_WORDS_5:
 
-            await message.answer(f''
-                                 f' {w}', parse_mode="HTML")
-            users["{0}".format(message.chat.id)]['attempts'] -= 1
+                # Шаблон для удаления букв в случае повторяющихся букв в слове-загадке
+                mistery: str = users["{0}".format(message.chat.id)][
+                    'secret_word']
 
-        if users["{0}".format(message.chat.id)]['attempts'] == 0:
+                # Слово-ответ с подсветкой
+                w = ''
+                for letter1, letter2 in zip(users["{0}".format(message.chat.id)]['secret_word'],
+                                            str(message.text).upper()):
+                    if letter1 == letter2:
+                        w += '<u>' + letter2.upper() + '</u>'
+                        mistery = mistery.replace(letter2.upper(), '', 1)
+                    elif letter2 in mistery and str(message.text).upper()[mistery.index(letter2)] != \
+                            users["{0}".format(message.chat.id)]['secret_word'][mistery.index(letter2)]:
+                        w += '<b>' + letter2.upper() + '</b>'
+                        mistery = mistery.replace(letter2.upper(), '', 1)
+                    else:
+                        w += '<s>' + letter2.upper() + '</s>'
+
+                await message.answer(f' {w}', parse_mode="HTML")
+                users["{0}".format(message.chat.id)]['temp_attempts'] -= 1
+            else:
+                await message.answer(f'Такого слова нет.'
+                                     f'Осталось {users["{0}".format(message.chat.id)]["temp_attempts"]} попыток.')
+
+        if users["{0}".format(message.chat.id)]['temp_attempts'] == 0:
             await message.answer(f'К сожалению, у вас больше не осталось '
                                  f'попыток. Вы проиграли :(\n\nМое слово '
                                  f'было {users["{0}".format(message.chat.id)]["secret_word"]}\n\nДавайте '
