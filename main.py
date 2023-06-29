ff0190d82691f0718ded9cd165f84b27114696b1
aslanova8 <aslanova.polina@inbox.ru>
2023-06-29 19:59:55 +0300

    Connect to db

--- main.py
+++ main.py
@@ -1,53 +1,113 @@
 import random
+import sqlite3
 
 from aiogram import Bot, Dispatcher, executor
 from aiogram.types import Message
 
+import db_operations as db
 
 BOT_TOKEN: str = '5915056188:AAHIMEHYEG_sxcQyNEfIc5IB_y0of-Zzf3Y'
+
+
+def db_add_user(user_id: int, user_name: str, user_surname: str, username: str, length_of_words: int = 5,
+                total_attempts: int = 5):
+    guessing = False
+    played_games = 0
+    cursor.execute(f"SELECT COUNT(*) FROM users WHERE user_id={user_id}")
+    user_exists = cursor.fetchall()[0][0]
+    if not user_exists:
+        cursor.execute("INSERT INTO users (user_id, user_name, user_surname, username, length_of_words, total_attempts,"
+                       "remaining_attempts, guessing, played_games, wins) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ",
+                       (user_id, user_name, user_surname, username, length_of_words, total_attempts,
+                        guessing, played_games, played_games, guessing))
+        conn.commit()
+
+
+def db_get_length_of_words(user_id: int) -> int:
+    cursor.execute("SELECT length_of_words FROM users WHERE user_id=?", (user_id,))
+    return cursor.fetchall()[0][0]
+
+
+def db_get_total_attempts(user_id: int) -> int:
+    cursor.execute("SELECT total_attempts FROM users WHERE user_id=?", (user_id,))
+    return cursor.fetchall()[0][0]
+
+
+def db_get_remaining_attempts(user_id: int) -> int:
+    cursor.execute("SELECT remaining_attempts FROM users WHERE user_id=?", (user_id,))
+    return cursor.fetchall()[0][0]
+
+
+def db_get_played_games(user_id: int) -> int:
+    cursor.execute("SELECT played_games FROM users WHERE user_id=?", (user_id,))
+    return cursor.fetchall()[0][0]
+
+
+def db_get_wins(user_id: int) -> int:
+    cursor.execute("SELECT wins FROM users WHERE user_id=?", (user_id,))
+    return cursor.fetchall()[0][0]
+
+
+def db_get_guessing(user_id: int) -> int:
+    cursor.execute("SELECT guessing FROM users WHERE user_id=?", (user_id,))
+    return cursor.fetchall()[0][0]
+
+
+def db_get_word(user_id: int) -> int:
+    cursor.execute("SELECT word FROM users WHERE user_id=?", (user_id,))
+    return cursor.fetchall()[0][0]
+
+
+def db_set_remaining_attempts(user_id: int, value: int) -> None:
+    cursor.execute("UPDATE users SET remaining_attempts=? WHERE user_id=?", (value, user_id))
+    conn.commit()
+
+
+def db_set_guessing(user_id: int, value: bool) -> None:
+    cursor.execute("UPDATE users SET guessing=? WHERE user_id=?", (value, user_id))
+    conn.commit()
+
+
+def db_set_wins(user_id: int, value: bool) -> None:
+    cursor.execute("UPDATE users SET wins=? WHERE user_id=?", (value, user_id))
+    conn.commit()
+
+
+def db_set_played_games(user_id: int, value: bool) -> None:
+    cursor.execute("UPDATE users SET played_games=? WHERE user_id=?", (value, user_id))
+    conn.commit()
+
+
+def db_set_word(user_id: int, value: bool) -> None:
+    cursor.execute("UPDATE users SET word=? WHERE user_id=?", (value, user_id))
+    conn.commit()
+
 
 # Создаем объекты бота и диспетчера
 bot: Bot = Bot(BOT_TOKEN)
 dp: Dispatcher = Dispatcher(bot)
 
-# Кортежи со словами
-words = list()
-for ind, length in enumerate(range(4, 9)):
-    with open('words/words_len_' + str(length), 'r', encoding="utf-8") as f:
-        words.append(tuple(line.strip() for line in f.readlines()))
+# Подключение к бд
+conn = sqlite3.connect('database/game_db.db', check_same_thread=False)
+cursor = conn.cursor()
 
-# Словарь, в котором будут храниться данные пользователей
-users: dict = {}
-
-
-def get_words_with_length(word_length: int) -> tuple:
-    """
-    Получить кортеж слов длины len.
-    word_length-4, т.к. минимальная длина угадываемого слова равна 4.
-    Параметры
-    ---------
-
-    word_length: int
-         Длина слова
-
-    Возвращаемое значение
-    ---------------------
-    tuple: кортеж слов длины len
-    """
-    return words[word_length-4]
+# Слова
+words = [tuple() for _ in range(9)]
+for length in range(4, 9):
+    with open(f'words/words_len_{length}.txt', 'r', encoding='utf-8') as f:
+        words[length] = words[length] + tuple(line.strip() for line in f.readlines())
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: Message):
-    if users.get("{0}".format(message.chat.id), False) is False:
-        users["{0}".format(message.chat.id)] = {'in_game': False,
-                                                'secret_word': None,
-                                                'total_attempts': 5,
-                                                'temp_attempts': 5,
-                                                'word_len': 5,
-                                                'total_games': 0,
-                                                'wins': 0}
+    us_id = message.from_user.id
+    us_name = message.from_user.first_name
+    us_sname = message.from_user.last_name
+    username = message.from_user.username
+
+    db_add_user(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
+
     await message.answer('Привет!\nДавай сыграем в игру "Угадай слово"?\n\n'
                          'Чтобы получить правила игры и список доступных '
                          'команд - отправьте команду /help')
@@ -57,8 +117,8 @@ async def process_start_command(message: Message):
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: Message):
     await message.answer(f'Правила игры:\n\nЯ загадываю слово из '
-                         f'{users["{0}".format(message.chat.id)]["word_len"]} букв, '
-                         f'а вам нужно его угадать\nУ вас есть {users["{0}".format(message.chat.id)]["total_attempts"]}'
+                         f'{db_get_length_of_words(message.from_user.id)} букв, '
+                         f'а вам нужно его угадать\nУ вас есть {db_get_total_attempts(message.from_user.id)}'
                          f'попыток\n\nДоступные команды:\n/help - правила '
                          f'игры и список команд\n/cancel - выйти из игры\n'
                          f'/stat - посмотреть статистику\n\nДавай сыграем?')
@@ -67,17 +127,17 @@ async def process_help_command(message: Message):
 # Этот хэндлер будет срабатывать на команду "/stat"
 @dp.message_handler(commands=['stat'])
 async def process_stat_command(message: Message):
-    await message.answer(f'Всего игр сыграно: {users["{0}".format(message.chat.id)]["total_games"]}\n'
-                         f'Игр выиграно: {users["{0}".format(message.chat.id)]["wins"]}')
+    await message.answer(f'Всего игр сыграно: {db_get_played_games(message.from_user.id)}\n'
+                         f'Игр выиграно: {db_get_wins(message.from_user.id)}')
 
 
 # Этот хэндлер будет срабатывать на команду "/cancel"
 @dp.message_handler(commands=['cancel'])
 async def process_cancel_command(message: Message):
-    if users["{0}".format(message.chat.id)]['in_game']:
+    if db_get_guessing(message.from_user.id):
         await message.answer('Вы вышли из игры. Если захотите сыграть '
                              'снова - напишите об этом')
-        users["{0}".format(message.chat.id)]['in_game'] = False
+        db_set_guessing(message.from_user.id, False)
     else:
         await message.answer('А мы итак с вами не играем. '
                              'Может, сыграем разок?')
@@ -90,16 +150,16 @@ async def process_cancel_command(message: Message):
 # TODO Система подсказок
 async def process_positive_answer(message: Message):
     await message.answer('Ура!\n\nЯ загадал слово, попробуй угадать!')
-    users["{0}".format(message.chat.id)]['in_game'] = True
-    users["{0}".format(message.chat.id)]['secret_word'] = random.choice(get_words_with_length(5))
-    users["{0}".format(message.chat.id)]['temp_attempts'] = users["{0}".format(message.chat.id)]["total_attempts"]
+    db_set_guessing(message.from_user.id, True)
+    db_set_word(message.from_user.id, random.choice(words[5]))
+    db_set_remaining_attempts(message.from_user.id, db_get_total_attempts(message.from_user.id))
 
 
 # Этот хэндлер будет срабатывать на отказ пользователя сыграть в игру
 @dp.message_handler(lambda message: message.text.upper() in ['НЕТ', 'НЕ', 'НЕ ХОЧУ', 'НЕ БУДУ', 'ВЫХОД'])
 async def process_negative_answer(message: Message):
-    if users["{0}".format(message.chat.id)]['in_game']:
-        await message.answer(f'Было загадано слово {users["{0}".format(message.chat.id)]["secret_word"]}.'
+    if db_get_guessing(message.from_user.id):
+        await message.answer(f'Было загадано слово {db_get_word(message.from_user.id)}.'
                              f'\n\nЕсли захотите поиграть - просто напишите об этом!')
     else:
         await message.answer('Жаль :(\n\nЕсли захотите поиграть - просто напишите об этом')
@@ -107,54 +167,53 @@ async def process_negative_answer(message: Message):
 
 # Этот хэндлер будет срабатывать на отправку пользователем слов нужной длины
 @dp.message_handler(
-    lambda message: message.text.isalpha() and len(message.text) == users["{0}".format(message.chat.id)]["word_len"])
+    lambda message: message.text.isalpha() and len(message.text) == db_get_length_of_words(message.from_user.id))
 async def process_word_answer(message: Message):
-    if users["{0}".format(message.chat.id)]['in_game']:
+    if db_get_guessing(message.from_user.id):
 
         # Победа
-        if str(message.text).upper() == users["{0}".format(message.chat.id)]['secret_word']:
+        if str(message.text).upper() == db_get_word(message.from_user.id):
             await message.answer('Ура!!! Вы угадали слово!\n\n'
                                  'Может, сыграем еще?')
-            users["{0}".format(message.chat.id)]['in_game'] = False
-            users["{0}".format(message.chat.id)]['total_games'] += 1
-            users["{0}".format(message.chat.id)]['wins'] += 1
-
+            db_set_guessing(message.from_user.id, False)
+            db_set_played_games(message.from_user.id, db_get_played_games(message.from_user.id) + 1)
+            db_set_wins(message.from_user.id, db_get_wins(message.from_user.id) + 1)
         # Попытка
         else:
             # Проверка слова на существование
-            if str(message.text).upper() in get_words_with_length(5):
+            if str(message.text).upper() in words[5]:
 
                 # Шаблон для удаления букв в случае повторяющихся букв в слове-загадке
-                mistery: str = users["{0}".format(message.chat.id)][
-                    'secret_word']
+                mistery: str = db_get_word(message.from_user.id)
 
                 # Слово-ответ с подсветкой
                 w = ''
-                for letter1, letter2 in zip(users["{0}".format(message.chat.id)]['secret_word'],
-                                            str(message.text).upper()):
+                for letter1, letter2 in zip(db_get_word(message.from_user.id), str(message.text).upper()):
                     if letter1 == letter2:
                         w += '<u>' + letter2.upper() + '</u>'
                         mistery = mistery.replace(letter2.upper(), '', 1)
                     elif letter2 in mistery and str(message.text).upper()[mistery.index(letter2)] != \
-                            users["{0}".format(message.chat.id)]['secret_word'][mistery.index(letter2)]:
+                            db_get_word(message.from_user.id)[mistery.index(letter2)]:
                         w += '<b>' + letter2.upper() + '</b>'
                         mistery = mistery.replace(letter2.upper(), '', 1)
                     else:
                         w += '<s>' + letter2.upper() + '</s>'
 
                 await message.answer(f' {w}', parse_mode="HTML")
-                users["{0}".format(message.chat.id)]['temp_attempts'] -= 1
+                db_set_remaining_attempts(message.from_user.id,
+                                             db_get_remaining_attempts(message.from_user.id) - 1)
+
             else:
                 await message.answer(f'Такого слова нет. '
-                                     f'Осталось {users["{0}".format(message.chat.id)]["temp_attempts"]} попыток.')
+                                     f'Осталось {db_get_remaining_attempts(message.from_user.id)} попыток.')
 
-        if users["{0}".format(message.chat.id)]['temp_attempts'] == 0:
+        if db_get_remaining_attempts(message.from_user.id) == 0:
             await message.answer(f'К сожалению, у вас больше не осталось '
                                  f'попыток. Вы проиграли :(\n\nМое слово '
-                                 f'было {users["{0}".format(message.chat.id)]["secret_word"]}\n\nДавайте '
+                                 f'было {db_get_word(message.from_user.id)}\n\nДавайте '
                                  f'сыграем еще?')
-            users["{0}".format(message.chat.id)]['in_game'] = False
-            users["{0}".format(message.chat.id)]['total_games'] += 1
+            db_set_guessing(message.from_user.id, False)
+            db_set_played_games(message.from_user.id, db_get_played_games(message.from_user.id) + 1)
     else:
         await message.answer('Мы еще не играем. Хотите сыграть?')
 
@@ -162,7 +221,7 @@ async def process_word_answer(message: Message):
 # Этот хэндлер будет срабатывать на остальные любые сообщения
 @dp.message_handler()
 async def process_other_text_answers(message: Message):
-    if users["{0}".format(message.chat.id)]['in_game']:
+    if db_get_guessing(message.from_user.id):
         await message.answer('Мы же сейчас с вами играем. '
                              'Присылайте, пожалуйста, слово.')
     else:
